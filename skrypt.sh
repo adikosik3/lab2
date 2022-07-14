b617fb1dde6a0c55c1c17fd59cef18ad1de3da31
adikosik3 <101297122+adikosik3@users.noreply.github.com>
2022-07-14 15:21:19 +0200

    Add files via upload

--- skrypt.sh
+++ skrypt.sh
@@ -2,6 +2,10 @@
 if [ $1 = "--help" ] ; then
 echo test
 fi
+if [ $1 = "--init" ] ; then
+git clone https://github.com/adikosik3/lab4.git
+
+fi
 if [ $1 = "--date" ] ; then
 date
 fi
@@ -15,6 +19,19 @@ else
 x=1;
 while [ $x -le 100 ] ; do
     echo log$x.txt skrypt.sh $(date) >> log$x.txt
+    x=$[x + 1]
+done
+fi
+if [[ $1 = "--error" ]] && [[ -n $2 ]] ; then
+x=1;
+while [ $x -le $2 ] ; do
+    echo errorx/errorx.txt skrypt.sh $(date) >> errorx/errorx.txt
+    x=$[x + 1]
+done
+else
+x=1;
+while [ $x -le 100 ] ; do
+    echo errorx/errorx/errorx.txt skrypt.sh $(date) >> errorx/errorx.txt
     x=$[x + 1]
 done
 fi
